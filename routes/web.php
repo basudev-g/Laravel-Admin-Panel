123ea8599973ab590f654dd1bd0615f44596bbfd
basudev-g <flancer.basudev@gmail.com>
2023-03-27 15:44:23 +0600

    Theme placed successfully

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('admin.index');
 });
+
+Route::get('settings', function () {
+    return view('admin.settings');
+})->name('admin-settings');
